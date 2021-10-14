9392ca72891dab0d091620753298d04ebddfedf6
MartinKarkovich <martin.karkovich@gmail.com>
2021-10-14 01:24:34 -0300

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2021_10_11_233853) do
 
-  create_table "clients", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "id", null: false
-    t.string "email", null: false
+  create_table "clients", primary_key: "client_id", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email"
     t.string "first_name"
     t.string "last_name"
     t.string "job"
@@ -24,37 +23,39 @@ ActiveRecord::Schema.define(version: 2021_10_11_233853) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["id"], name: "index_clients_on_id", unique: true
   end
 
-  create_table "discounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "discounts", primary_key: "discount_id", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "payment_id", null: false
     t.integer "amount"
-    t.integer "type"
-    t.bigint "payment_id"
+    t.integer "discount_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["payment_id"], name: "index_discounts_on_payment_id"
   end
 
-  create_table "payments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "payments", primary_key: "payment_id", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "client_id", null: false
     t.string "currency"
     t.integer "full_amount"
-    t.integer "discounts"
+    t.integer "full_discounts"
     t.integer "total"
     t.datetime "date"
-    t.bigint "client_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["client_id"], name: "index_payments_on_client_id"
   end
 
-  create_table "transactions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "transactions", primary_key: "transaction_id", id: :string, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "payment_id", null: false
     t.integer "amount"
-    t.integer "type"
-    t.bigint "payment_id"
+    t.integer "transaction_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["payment_id"], name: "index_transactions_on_payment_id"
   end
 
+  add_foreign_key "discounts", "payments", primary_key: "payment_id"
+  add_foreign_key "payments", "clients", primary_key: "client_id"
+  add_foreign_key "transactions", "payments", primary_key: "payment_id"
 end
